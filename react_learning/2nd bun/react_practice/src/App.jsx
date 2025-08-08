126720a88bde7d3070e82ff9a4e983538101af9a
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-08 15:07:33 +0500

    card

--- react_learning/2nd bun/react_practice/src/App.jsx
+++ react_learning/2nd bun/react_practice/src/App.jsx
@@ -1,5 +1,9 @@
 export const App = () =>{
   return(
-    <h1>hello umer</h1>
+<>
+<h1> Dirilis Ertugrul Seasons 1</h1>
+<p>Diriliş: Ertuğrul Season 1 revolves around Ertuğrul Bey, leader of the Kayi tribe, as he battles the Crusaders, Mongols, and internal enemies. It highlights his leadership, honor, loyalty, and struggles to protect his people. Ertuğrul forms strategic alliances, confronts betrayal, and seeks justice while preparing for the rise of the Ottoman Empire.</p>
+<img height={"200px"} width={"300px"} w src="s-l1200.jpg" alt="" />
+</>
   )
 }
