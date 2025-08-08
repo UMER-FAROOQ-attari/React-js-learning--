b7900ad4e45809718d1a0269fc791561f3bbe0b0
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-08 15:37:23 +0500

    Dynamic Values in JSX

--- react_learning/2nd bun/react_practice/src/App.jsx
+++ react_learning/2nd bun/react_practice/src/App.jsx
@@ -18,11 +18,20 @@ export const App = () =>{
   )
 }
 const Card=()=>{
-  return(
+ const name = "Dirilis Ertugrul Seasons 1"
+ const sumry = "Diriliş: Ertuğrul Season 1 revolves around Ertuğrul Bey, leader of the Kayi tribe, as he battles the Crusaders, Mongols, and internal enemies. It highlights his leadership, honor, loyalty, and struggles to protect his people. Ertuğrul forms strategic alliances, confronts betrayal, and seeks justice while preparing for the rise of the Ottoman Empire."
+
+ const by = ()=>{
+  const byu = "UMER"
+  return byu
+ }
+return(
 <>
-<h1> Dirilis Ertugrul Seasons 1</h1>
-<p>Diriliş: Ertuğrul Season 1 revolves around Ertuğrul Bey, leader of the Kayi tribe, as he battles the Crusaders, Mongols, and internal enemies. It highlights his leadership, honor, loyalty, and struggles to protect his people. Ertuğrul forms strategic alliances, confronts betrayal, and seeks justice while preparing for the rise of the Ottoman Empire.</p>
+<p>{8*8-63}</p>
+<h1> NAME : {name} </h1>
+<p> SUMMARY :{sumry}</p>
 <img height={"200px"} width={"300px"} w src="s-l1200.jpg" alt="" />
+<h5>BY:{by()}</h5>
 </>
   )
 }
