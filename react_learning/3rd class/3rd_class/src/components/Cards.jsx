bcddf8ee52d6bb0371be6b3626e974fe42e69aae
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-09 16:51:12 +0500

    cards

---
+++ react_learning/3rd class/3rd_class/src/components/Cards.jsx
@@ -0,0 +1,29 @@
+const Card=()=>{
+ const name = "Dirilis Ertugrul Seasons 1"
+ const sumry = "Diriliş: Ertuğrul Season 1 revolves around Ertuğrul Bey, leader of the Kayi tribe, as he battles the Crusaders, Mongols, and internal enemies. It highlights his leadership, honor, loyalty, and struggles to protect his people. Ertuğrul forms strategic alliances, confronts betrayal, and seeks justice while preparing for the rise of the Ottoman Empire."
+
+ const by = ()=>{
+  const byu = "UMER"
+  return byu
+ }
+ const age = 19;
+
+ const btn = ()=>{
+ if(age >= 18) return "Watch Now";
+  return "Not Available";
+ }
+//  let canwatch = "Not Available"
+//  if(age>18) canwatch = "Watch Now"
+return(
+<>
+<p>{8*8-63}</p>
+<h1> NAME : {name} </h1>
+<p> SUMMARY :{sumry}</p>
+<img height={"200px"} width={"300px"} w src="s-l1200.jpg" alt="" />
+<h5>BY:{by()}</h5>
+{/* <button>{age>18? "Watch Now" : "Not Available"}</button> */}
+{/* <button>{canwatch}</button> */}
+<button>{btn()}</button>
+</>
+  )
+}
